d91dc7fc2fabe013dd94d99485591f9c2325eaee
Filip Saina <fsaina@croz.net>
2016-07-10 14:13:39 +0200

    Login screen implemented as stated in the hw01

---
+++ Pokemaster/RootViewController.swift
@@ -0,0 +1,51 @@
+//
+//  RootViewController.swift
+//  Pokemaster
+//
+//  Created by Filip Saina on 04/07/16.
+//  Copyright © 2016 InfinumAcademy. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * Login view controller class. Contains all the necessary logic for
+ * handling the login process.
+ */
+class RootViewController: UIViewController {
+    
+    // refference to the email text field
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    // refference to the login button field
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // refference password text filed
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.loginButton.setTitle("Test 123", forState: UIControlState.Normal)
+        loginButton.addTarget(self, action: #selector(RootViewController.loginButtonClicked), forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    // Touch button handler for the login button
+    func loginButtonClicked(){
+        
+        if emailTextField.text != nil{
+            print("Email field: " + emailTextField.text!)
+        }
+        
+        if(passwordTextField.text != nil){
+            print("Password field: " + passwordTextField.text!)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
