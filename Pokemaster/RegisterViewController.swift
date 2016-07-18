1784950bf804f5912d45b5c328cadbc29e398207
Filip Saina <fsaina@croz.net>
2016-07-18 16:25:29 +0200

    Register screen UI components added

--- Pokemaster/RegisterViewController.swift
+++ Pokemaster/RegisterViewController.swift
@@ -13,12 +13,37 @@ import UIKit
  * handling the login process.
  */
 class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
 
+    @IBOutlet weak var nicknameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var navigationViewBackButton: UINavigationItem!
     
     //Entry point of the view
     override func viewDidLoad() {
-        //SOME code
+        
+        // set underlying borders
+        emailTextField.setBottomBorderTextField()
+        nicknameTextField.setBottomBorderTextField()
+        passwordTextField.setBottomBorderTextField()
+        confirmPasswordTextField.setBottomBorderTextField()
+        
+        // set images with every textfield
+        emailTextField.setTextFieldLeftIcon("mail.png")
+        nicknameTextField.setTextFieldLeftIcon("user.png")
+        passwordTextField.setTextFieldLeftIcon("lock.png")
+        confirmPasswordTextField.setTextFieldLeftIcon("lock.png")
+    
     }
     
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        //send POST request to server
+    }
 
 }
